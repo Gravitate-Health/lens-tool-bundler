46a641b24e9a74f9ffcd90dd6311f739bb97084a
Alejandro M. Medrano Gil <amedrano@lst.tfo.upm.es>
2026-02-01 17:10:19 +0100

    Updated dependencies for improved linting support

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import oclifConfig from 'eslint-config-oclif';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettier from 'eslint-config-prettier';
+
+export default [
+  {
+    ignores: ['dist/**', 'lib/**', 'node_modules/**', '*.d.ts']
+  },
+  ...oclifConfig,
+  {
+    files: ['**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+        project: './tsconfig.json'
+      },
+      globals: {
+        console: 'readonly',
+        process: 'readonly',
+        Buffer: 'readonly',
+        __dirname: 'readonly',
+        __filename: 'readonly',
+        module: 'readonly',
+        require: 'readonly'
+      }
+    },
+    plugins: {
+      '@typescript-eslint': tseslint
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }]
+    }
+  },
+  prettier
+];
